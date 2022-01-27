433c5a1b3f2951711d70f4112ded175d628fae83
Spotts91 <oakshassa864@gmail.com>
2022-01-26 19:15:37 -0800

    finalized MVC file structuring for review

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
-
-// create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('tic_tac_toe_vocab', 'username', 'password', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
