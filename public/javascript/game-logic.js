08841b616b2d3dc7e6591241a8cec1a279905fc8
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-02-01 07:46:17 -0800

    pseudocoding game logic, refactored CSS

---
+++ public/javascript/game-logic.js
@@ -0,0 +1,27 @@
+// win conditions function
+//      list of possible winning combinations
+
+const winCombos = [
+	[0, 1, 2], //
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2],
+];
+
+const tableCells = document.querySelectorAll('.table-cells');
+startGame();
+
+function startGame() {
+
+};
+//      if one of these is met, game over
+
+//      else game keeps going
+
+// pictures and words match/don't match function
+
+// reset board at end of game function
