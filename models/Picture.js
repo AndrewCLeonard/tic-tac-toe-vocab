011bcb582c852ec379bc2be37a571b2c1c49ca45
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-02-06 08:12:58 -0800

    add models/Picture.js

---
+++ models/Picture.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+const Picture = sequelize.define('picture', {
+    wordId: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    word: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    wordDescription: { 
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    pictureId: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+});
+
+//`sequelize.define` also returns the model
+console.log(Picture === sequelize.models.Picture); //true
+
+module.exports = Picture;
