2cbf5d5375cdafcbd4a2e968cd26ae6124ab3a4a
Spotts91 <oakshassa864@gmail.com>
2022-02-26 09:10:36 -0800

    login authorization fixed and running, needs to be merged

--- models/Player.js
+++ models/Player.js
@@ -3,56 +3,60 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection.js");
 
 // create Player model
-const Player = sequelize.define(
-	"Player",
+class Player extends Model {
+	checkPassword(loginPW) {
+		return bcrypt.compareSync(loginPW, this.password);
+	}
+}
+
+Player.init(
 	{
 		playerId: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
 			primaryKey: true,
-			autoIncrement: true,
-		},
+			autoIncrement: true
+	},
 		username: {
 			type: DataTypes.STRING,
 			unique: true,
-			allowNull: false,
+			allowNull: false
 			// Other model options go here
-		},
+	},
 		firstname: {
 			type: DataTypes.STRING,
-			allowNull: false,
-		},
+			allowNull: false
+	},
 		lastname: {
 			type: DataTypes.STRING,
-			allowNull: false,
+			allowNull: false
 			// allowNull defaults to true
-		},
+	},
 		password: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				len: [4],
-			},
+			len: [4]
+		}
+	}
+},
+{
+	hooks: {
+		// set up beforeCreate lifecycle "hook" functionality
+		async beforeCreate(newPlayerData) {
+			newUserData.password = await bcrypt.hash(newPlayerData.password, 10);
+			return newPlayerData;
 		},
+		async beforeUpdate(updatedPlayerData) {
+			updatedPlayerData.password = await bcrypt.hash(updatedPlayerData.password, 10);
+			return updatedPlayerData;
+		}
 	},
-	{
-		hooks: {
-			// set up beforeCreate lifecycle "hook" functionality
-			async beforeCreate(newPlayerData) {
-				newUserData.password = await bcrypt.hash(newPlayerData.password, 10);
-				return newPlayerData;
-			},
-
-			async beforeUpdate(updatedPlayerData) {
-				updatedPlayersData.password = await bcrypt.hash(updatedPlayerData.password, 10);
-				return updatedPlayerData;
-			},
-		},
 		sequelize,
 		timestamps: false,
 		freezeTableName: true,
 		underscored: true,
-		modelName: "user",
+		modelName: "player",
 	}
 );
 
